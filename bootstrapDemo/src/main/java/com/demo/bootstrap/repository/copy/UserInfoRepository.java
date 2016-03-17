32889fbde349da55671217085abc7d190fbc9573
jyh <YongHoon.Ji@systrangroup.com>
2016-03-17 18:58:15 +0900

    .

---
+++ bootstrapDemo/src/main/java/com/demo/bootstrap/repository/copy/UserInfoRepository.java
@@ -0,0 +1,16 @@
+package com.demo.bootstrap.repository.copy;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.demo.bootstrap.domain.UserInfo;
+
+// JpaRepository extends PagingAndSortingRepository , which in turn extends CrudRepository.
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, String> {
+	List<UserInfo> findAllByName(String email, Pageable pageable);
+	UserInfo findByIdAndPassWord(String email, String password);
+}
